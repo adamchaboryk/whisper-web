77ca5f9f6de59b0cc076a486a4373bc291d77334
Adam Chaboryk <adam@chaboryk.xyz>
2026-08-22 20:03:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
+  base: '/whisper-web/',
   experimental: {
     bundledDev: false,
   },
